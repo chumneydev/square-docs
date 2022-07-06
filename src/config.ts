6bf889ccd1033700197ff0d33da48349819649f3
Louis Stephens <lstephens@chumneyads.com>
2022-07-06 16:04:44 -0400

    moved intro to base

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const SidebarConfig = {
+    pages: [
+        { text: '', header: true },
+        { text: 'Section Header', header: true },
+        { text: 'Introduction',  link: '/getting-started/introduction' },
+
+
+    ]
+
+}
