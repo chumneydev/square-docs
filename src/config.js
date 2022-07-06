8741d0b96bcaf20d6dac2dbd4aa35190819c7f52
Louis Stephens <lstephens@chumneyads.com>
2022-07-06 17:41:18 -0400

    adding sidebar

---
+++ src/config.js
@@ -0,0 +1,31 @@
+export const SidebarConfig = {
+    pages: [
+        { text: '', header: true },
+        { text: 'Getting Started', header: true },
+        { text: 'Introduction',  link: '/getting-started/introduction' },
+        { text: 'Test',  link: '/getting-started/test' },
+
+        { text: 'Layout', header: true },
+        { text: 'Introduction',  link: '/getting-started/introduction' },
+        { text: 'Test',  link: '/getting-started/test' },
+
+        { text: 'Forms', header: true },
+        { text: 'Introduction',  link: '/getting-started/introduction' },
+        { text: 'Test',  link: '/getting-started/test' },
+
+        { text: 'Components', header: true },
+        { text: 'Introduction',  link: '/getting-started/introduction' },
+        { text: 'Test',  link: '/getting-started/test' },
+
+        { text: 'Helpers', header: true },
+        { text: 'Introduction',  link: '/getting-started/introduction' },
+        { text: 'Test',  link: '/getting-started/test' },
+
+        { text: 'Utilities', header: true },
+        { text: 'Introduction',  link: '/getting-started/introduction' },
+        { text: 'Test',  link: '/getting-started/test' },
+
+
+    ]
+
+}
